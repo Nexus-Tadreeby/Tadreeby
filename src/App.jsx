2e261e318b2bca1bbd99c58168bd80e094e9ba2d
afnankullab-dev <afnankullab3@gmail.com>
2026-06-29 19:05:00 +0300

    refactor(app): rout handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import { RegistrationPage } from "./components/pages/RegistrationPage";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LoginPage } from "./components/auth/LoginPage";
+import { RegistrationPage } from "./components/auth/RegistrationPage";
+import { ForgotPasswordPage } from "./components/auth/ForgotPasswordPage";
+import { CheckEmailPage } from "./components/auth/CheckEmailPage";
+import { ResetPasswordPage } from "./components/auth/ResetPasswordPage";
+import { SuccessPasswordReset } from "./components/auth/SuccessPasswordReset";
+import {LandingPage} from "./components/pages/LandingPage";
+
 
 function App() {
-  return <RegistrationPage />;
+    return (
+    <BrowserRouter>
+      <Routes>  
+        <Route path="/" element={<RegistrationPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/check-email" element={<CheckEmailPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route path="/reset-success" element={<SuccessPasswordReset />} />
+        <Route path="/landing" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
+  ); 
 }
 
 export default App;

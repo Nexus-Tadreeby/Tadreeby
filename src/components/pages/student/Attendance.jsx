cf47d23fe45e52da075dade6849a439626328e48
shahd <eng.shahd.abusharif@gmail.com>
2026-09-03 13:36:54 +0300

    update `Browse Opportunity` btn margin

--- src/components/pages/student/Attendance.jsx
+++ src/components/pages/student/Attendance.jsx
@@ -266,7 +266,7 @@ const ActiveSessionBanner = ({
                         <Button
                             variant="blue"
                             onClick={onBrowseOpportunities}
-                            className="mt-2 px-4 py-2 text-[11px]"
+                            className="mt-2 my-4 mx-15 px-4 py-2 text-[11px]"
                         >
                             Browse Opportunities
                         </Button>
